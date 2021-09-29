6806327c025a60b58f77d0a3239e6c0dec4834fa
rzarandona <rein.zarandona@gmail.com>
2021-09-29 21:31:28 +0800

    tests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <h1>Homepage {{base_url}}</h1>
-    <h1>Hello, World!</h1>
+    <h1>We are traction</h1>
   </div>
 </template>
 
 <script>
 export default {
-  computed:{
-    base_url(){
-      return process.env.VUE_APP_BASE_URL
-    }
-  }
-}
+  computed: {
+    base_url() {
+      return process.env.VUE_APP_BASE_URL;
+    },
+  },
+};
 </script>
